8a0d6fa3468fd1f72766fb090dfe0840a404ae29
oes <munandardotcom@gmail.com>
2023-10-25 13:59:29 +0700

    basic javascript

---
+++ basic.js
@@ -0,0 +1,21 @@
+var a = 5;
+
+var b = 3;
+
+ 
+
+// menggunakan operator penjumlahan
+
+var c = a + b;
+
+console.log(c);
+
+ 
+
+Operator penggabungan teks (hanya string)
+
+var a = "10" + "gabung";
+
+ 
+
+a = "10gabung" //string
